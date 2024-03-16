655d56e99278927db4cbfe0dff283b1b311d04bc
HugoSwen <2407475450@qq.com>
2024-03-16 22:03:18 +0800

    登录接口修改

--- server/common/login.py
+++ server/common/login.py
@@ -9,13 +9,13 @@ from tortoise.exceptions import DoesNotExist
 login = APIRouter()
 
 
-@login.get("/login")
-async def login(userDTO: UserDTO):
+@login.post("/login")
+async def user_login(userDTO: UserDTO):
     try:
-        user = await User.get(userNumber=userDTO.userNumber)
+        user = await User.get(userNumber=userDTO.userNumber, password=userDTO.password, role=userDTO.role)
         # TODO JWT Token生成
-        token = ''
+        token = '这是token'
         return Result.success(LoginVO(id=user.id, token=token))
-    except DoesNotExist as e:
+    except DoesNotExist:
         # TODO 返回400状态码
         return Result.error("账号或密码错误！")
