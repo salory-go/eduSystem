3652ba75546a16044440bfb438b3d798405784be
HugoSwen <2407475450@qq.com>
2024-03-23 10:08:30 +0800

    设置Api返回文本格式

--- util/chatglmApi.py
+++ util/chatglmApi.py
@@ -2,7 +2,7 @@ import time
 from typing import List
 from zhipuai import ZhipuAI
 
-from const.msg import reference_msg, eval_msg, question_msg
+from const.msg import reference_msg, eval_msg, question_msg, diff_map
 
 client = ZhipuAI(api_key="7b1516a49710d181bb671927898a14b4.OCE2vCoySXStxYzx")
 
@@ -18,14 +18,8 @@ def print_with_typewriter_effect(text, delay=0.05):
 #         content = chunk.choices[0].delta.content
 #         print_with_typewriter_effect(content)
 
-difficulty_map = {
-    1: "简单",
-    2: "中等",
-    3: "困难"
-}
 
-
-def create(message: List[dict], top_p: float = 0.7, temperature: float = 0.5, max_tokens: int = 2000):
+def create(message: List[dict], top_p: float = 0.7, temperature: float = 0.9, max_tokens: int = 2000):
     res = client.chat.completions.create(
         model="glm-4",
         messages=message,
@@ -44,11 +38,10 @@ def generate_ideas(content: str, courseName: str, chapterName: str):
             "content": reference_msg % (content, courseName, chapterName)
         }
     ]
-    res = create(messages)
-    return res
+    return create(messages)
 
 
-def generate_eval(content: str, studentAnswer: str, answer):
+def generate_eval(content: str, studentAnswer: str, answer: str):
     messages = [
         {"role": "system", "content": "你是一名计算机专业的大学教授。"},
         {
@@ -56,8 +49,7 @@ def generate_eval(content: str, studentAnswer: str, answer):
             "content": eval_msg % (content, studentAnswer, answer)
         }
     ]
-    res = create(messages)
-    return res
+    return create(messages)
 
 
 def generate_question(number: int, courseName: str, chapterName: str, difficulty: int):
@@ -65,12 +57,7 @@ def generate_question(number: int, courseName: str, chapterName: str, difficulty
         {"role": "system", "content": "你是一名计算机专业的大学教授。"},
         {
             "role": "user",
-            "content": question_msg % (number, courseName, chapterName, difficulty_map[difficulty])
+            "content": question_msg % (number, courseName, chapterName, diff_map[difficulty])
         }
     ]
-    res = create(messages)
-    return res
-
-
-# generate_ideas("在数据链路层中，什么是帧？请简述帧的主要作用。", "计算机网络", "数据链路层")
-generate_question(2, "计算机网络", "数据链路层", 2)
+    return create(messages)
