c0549d2811ff3bf9f007d56e7880a0ee747d40a0
HugoSwen <2407475450@qq.com>
2024-03-24 17:16:42 +0800

    增加jwt token生成和检验

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from fastapi import FastAPI, Request, Response
 from tortoise.contrib.fastapi import register_tortoise
 
 from const.dbConfig import TORTOISE_ORM
+from const.path import path
 from server.admin.router import admin
 from server.common.router import common
 from server.student.router import student
 from server.teacher.router import teacher
 from pojo.result import Result
+from util.jwtToken import verify
 
 app = FastAPI()
 
@@ -25,15 +27,18 @@ app.include_router(teacher, prefix='/teacher', tags=["教师接口"])
 app.include_router(student, prefix='/student', tags=["学生接口"])
 
 
-# @app.middleware("http")
-# async def m1(request: Request, call_next):
-#     if request.headers.get('token') is None:
-#         result = json.dumps(Result.error("未登录").model_dump())
-#         return Response(status_code=401, media_type="application/json", content=result)
-#
-#     response = await call_next(request)
-#     return response
+@app.middleware("http")
+async def m1(request: Request, call_next):
+    token = request.headers.get('token')
+    print(request.url.path)
+    if (request.url.path not in path and
+            (token is None or verify(token) is False)):
+        result = json.dumps(Result.error("请登录！").model_dump())
+        return Response(status_code=401, media_type="application/json", content=result)
+
+    response = await call_next(request)
+    return response
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", port=8000, reload=True, log_level="info")
