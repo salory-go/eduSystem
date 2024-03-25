aa8ac434bec71130fa8a5cd6a1cf21193f6670cf
HugoSwen <2407475450@qq.com>
2024-03-25 15:17:46 +0800

    定时任务增加日志

--- task/assignmentTask.py
+++ task/assignmentTask.py
@@ -2,14 +2,16 @@ from datetime import datetime
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from pojo.entity import Assignment
+from util.logger import logger
 
 scheduler = AsyncIOScheduler()
 
 
-@scheduler.scheduled_job(trigger=IntervalTrigger(minutes=5))
+@scheduler.scheduled_job(trigger=IntervalTrigger(seconds=5))
 async def check_overdue():
+    logger.info("check overdue assignments...")
+
     assignments = await Assignment.filter(overdue=False).values('id', 'deadline')
-    print(1)
     for a in assignments:
-        if a['deadline'] < datetime.now():
+        if a['deadline'] < datetime.now().replace(microsecond=0):
             await Assignment.filter(id=a['id']).update(overdue=True)
